db796ec14a474fae16b2612442e0e8edbbc2e4a8
teknofage <teknofage@gmail.com>
2019-11-21 17:03:37 -0800

    Users can see single playlists

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.Playlister
 playlists = db.playlists
 
 app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     """Return homeppage."""
-#     return render_template('home.html', msg='Flask is Cool!!')
-    
-# Our mock arrary of projects
-# playlists = [
-#     {"title": "Cat Videos", "description": "Cats acting weird"},
-#     {"title": '80\'s Music', 'description': "Don\'t stop believing!" }
-# ]
 
 def video_url_creator(id_lst):
     videos = []
@@ -52,9 +42,14 @@ def playlists_submit():
         'videos': videos,
         'video_ids': video_ids
     }
-    playlists.insert_one(playlist)
-    return redirect(url_for('playlists_index'))
+    playlist_id = playlists.insert_one(playlist).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
+@app.route('/playlists/<playlist_id>', methods= ['GET'])
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({"_id": ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
 
 if __name__ == '__main__':
     app.run(debug=True)
