138803260808bb48edf95fd722aba7916871300e
teknofage <teknofage@gmail.com>
2019-12-02 23:26:18 -0800

    datestamped comments

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
+from datetime import datetime
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=host)
@@ -100,6 +101,7 @@ def comments_new():
         'title': request.form.get('title'),
         'content': request.form.get('content'),
         'playlist_id': ObjectId(request.form.get('playlist_id'))
+        'created_at': datetime.now()
     }
 
     print(comment)
